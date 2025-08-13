349fc7b95d278ac6d3b8dd38612ca06b4e70dc2a
Kwasi Dankwa <104780360+Kwasi-Dankwa@users.noreply.github.com>
2025-08-13 00:36:13 -0400

    Update vgquery.sql
    adding cleaning processes for the data

--- vgquery.sql
+++ vgquery.sql
@@ -34,7 +34,7 @@ CREATE TABLE video_game_sales (
     Global_Sales DECIMAL(10, 2) NULL
 );
 
--- EDA PHASE --
+-- Cleaning PHASE --
 
 -- total rows --
 SELECT count(*) 
@@ -69,4 +69,64 @@ where Global_Sales is null
 
 
 
--- next step will involve cleaning data --
+-- checking name duplicates --
+select name, year, count(name)
+from public.video_game_sales
+group by name, year
+having count(name) > 1
+
+select *
+from public.video_game_sales
+where year is null
+
+-- updating null year value which is crucial for trend analysis--
+UPDATE public.video_game_sales
+SET year = 2005
+WHERE name = 'Madden NFL 06' AND year IS NULL;
+
+UPDATE public.video_game_sales
+SET year = 2003
+WHERE name like '%2004%' AND year IS NULL;
+
+-- using self join to find games that dropped the same year and updates the null values--
+UPDATE public.video_game_sales AS g1
+SET year = g2.known_year
+FROM (
+    SELECT 
+        name, 
+        MAX(year) AS known_year
+    FROM public.video_game_sales
+    WHERE year IS NOT NULL
+    GROUP BY name
+) AS g2
+WHERE g1.name = g2.name AND g1.year IS NULL;
+
+-- i was able to update 121 rows with this query--
+-- other empty year columns are going to be dropped--
+
+delete from public.video_game_sales
+where year is null
+
+select *
+from public.video_game_sales
+where publisher is null
+
+--finding similar publishers from games with missing values to grab publishers from other rows with same name--
+UPDATE public.video_game_sales AS g1
+SET publisher = g2.known_pub
+FROM (
+    SELECT
+        name,
+        MAX(publisher) AS known_pub
+    FROM public.video_game_sales
+    WHERE publisher IS NOT NULL
+    GROUP BY name
+) AS g2
+WHERE g1.name = g2.name AND g1.publisher IS NULL;
+
+-- the other 35 row are going to be set to unknown --
+update public.video_game_sales
+set publisher = 'Unknown'
+where publisher is null;
+
+-- other rows don't have empty columns this ends the data cleaning/wrangling phase--
